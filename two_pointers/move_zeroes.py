0d71403d5e7d68f0f22628f40702fe3fd86c87ef
ruabbk9 <kirill.boblak@raiffeisen.ru>
2023-01-29 12:45:39 +0300

    move zeroes

---
+++ two_pointers/move_zeroes.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+def move_zeroes(self, nums: List[int]) -> None:
+    zero_index = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[zero_index], nums[i] = nums[i], nums[zero_index]
+            zero_index += 1
