784d2666cbafce8c9d7c12e1bd958e5f3576df2d
ruabbk9 <kirill.boblak@raiffeisen.ru>
2023-02-02 18:44:04 +0300

    flood_fill

---
+++ dfs/flood_fill.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def flood_fill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+    R, C = len(image), len(image[0])
+    old_color = image[sr][sc]
+    if old_color == color: return image
+
+    def dfs(r, c):
+        if image[r][c] == old_color:
+            image[r][c] = color
+            if r >= 1: dfs(r - 1, c)
+            if r + 1 < R: dfs(r + 1, c)
+            if c >= 1: dfs(r, c - 1)
+            if c + 1 < C: dfs(r, c + 1)
+
+    dfs(sr, sc)
+    return image
+
