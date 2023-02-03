89ce43ce54bbaa622b5728ba23bb180332320e1e
ruabbk9 <kirill.boblak@raiffeisen.ru>
2023-02-03 19:28:05 +0300

    merge_trees

---
+++ dfs/merge_binary_trees.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def merge_trees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+    if not root1:
+        return root2
+    if not root2:
+        return root1
+
+    root1.val += root2.val
+    root1.left = merge_trees(root1.left, root2.left)
+    root1.right = merge_trees(root1.right, root2.right)
+    return root1
