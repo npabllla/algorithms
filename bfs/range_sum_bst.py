7ce7b530584475e22cdba62fb6e18ba27d0f4bc6
ruabbk9 <kirill.boblak@raiffeisen.ru>
2023-02-07 22:25:11 +0300

    range_sum_bst

--- bfs/range_sum_bst.py
+++ bfs/range_sum_bst.py
@@ -18,3 +18,19 @@ def range_sum_bst(self, root: Optional[TreeNode], low: int, high: int) -> int:
                 queue.append(curr.right)
 
     return res
+
+
+def range_sum_bst_2(self, root: Optional[TreeNode], low: int, high: int) -> int:
+    queue = [root]
+    res = 0
+    while len(queue):
+        curr = queue.pop()
+        if curr:
+            if low <= curr.val <= high:
+                res += curr.val
+            if curr.left and curr.val > low:
+                queue.append(curr.left)
+            if curr.right and curr.val < high:
+                queue.append(curr.right)
+
+    return res
